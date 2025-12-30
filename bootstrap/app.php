9e91275e3beafd5f9bcc7c41fedda7a93d251f5e
ahmednour1234 <ahmednour5999@gmail.com>
2025-12-30 19:10:56 +0200

    www

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,3 +17,16 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Manually set the namespace if detection fails
+try {
+    $app->getNamespace();
+} catch (\RuntimeException $e) {
+    // Use reflection to set the namespace property
+    $reflection = new \ReflectionClass($app);
+    $property = $reflection->getProperty('namespace');
+    $property->setAccessible(true);
+    $property->setValue($app, 'App');
+}
+
+return $app;
