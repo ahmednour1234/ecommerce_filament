7b89826f5bf9f7498d2b655e72a5932bb14c7db8
ahmednour1234 <ahmednour5999@gmail.com>
2025-12-30 15:30:24 +0200

    wwww

--- app/Models/Accounting/JournalEntry.php
+++ app/Models/Accounting/JournalEntry.php
@@ -11,11 +11,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JournalEntry extends Model
 {
-    use HasBranch, HasCostCenter, SoftDeletes;
+    use HasBranch, HasCostCenter;
 
     protected $fillable = [
         'journal_id',
