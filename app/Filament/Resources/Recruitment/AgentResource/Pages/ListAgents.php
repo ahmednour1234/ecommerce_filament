c6910a387cee7ee222fb0f9d6dcae1ba4b222757
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-17 16:30:30 +0200

    5656

--- app/Filament/Resources/Recruitment/AgentResource/Pages/ListAgents.php
+++ app/Filament/Resources/Recruitment/AgentResource/Pages/ListAgents.php
@@ -2,18 +2,36 @@
 
 namespace App\Filament\Resources\Recruitment\AgentResource\Pages;
 
+use App\Filament\Concerns\ExportsResourceTable;
 use App\Filament\Resources\Recruitment\AgentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAgents extends ListRecords
 {
+    use ExportsResourceTable;
+
     protected static string $resource = AgentResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('export_excel')
+                ->label(tr('general.actions.export_excel', [], null, 'dashboard') ?: 'Export to Excel')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->action(fn () => $this->exportToExcel()),
+
+            Actions\Action::make('export_pdf')
+                ->label(tr('general.actions.export_pdf', [], null, 'dashboard') ?: 'Export to PDF')
+                ->icon('heroicon-o-document-arrow-down')
+                ->action(fn () => $this->exportToPdf()),
+
+            Actions\Action::make('print')
+                ->label(tr('general.actions.print', [], null, 'dashboard') ?: 'Print')
+                ->icon('heroicon-o-printer')
+                ->url(fn () => $this->getPrintUrl())
+                ->openUrlInNewTab(),
         ];
     }
 }
