59886d37e53d982c302831dd323df7c74bdadde1
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-21 06:48:17 +0200

    5656

--- app/Filament/Widgets/AccountingSummaryWidget.php
+++ app/Filament/Widgets/AccountingSummaryWidget.php
@@ -31,20 +31,7 @@ class AccountingSummaryWidget extends BaseWidget
             ->sum(fn ($account) => $accountingService->getAccountBalance($account->id));
 
         return [
-            Stat::make(tr('dashboard.stats.total_assets'), '$' . number_format($assets, 2))
-                ->description(tr('dashboard.stats.total_assets_description'))
-                ->color('success')
-                ->icon('heroicon-o-banknotes'),
 
-            Stat::make(tr('dashboard.stats.total_liabilities'), '$' . number_format($liabilities, 2))
-                ->description(tr('dashboard.stats.total_liabilities_description'))
-                ->color('danger')
-                ->icon('heroicon-o-exclamation-triangle'),
-
-            Stat::make(tr('dashboard.stats.total_equity'), '$' . number_format($equity, 2))
-                ->description(tr('dashboard.stats.total_equity_description'))
-                ->color('info')
-                ->icon('heroicon-o-chart-bar'),
         ];
     }
 }
