7255cc7a0415dc6000f301a2d053494e8e79a921
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-23 23:16:28 +0200

    qweqw

--- app/Filament/Resources/Recruitment/RecruitmentContractResource.php
+++ app/Filament/Resources/Recruitment/RecruitmentContractResource.php
@@ -648,6 +648,20 @@ class RecruitmentContractResource extends Resource
         ];
     }
 
+    public static function getUrl(string $name = 'index', array $parameters = [], bool $isAbsolute = true, ?string $panel = null, ?\Illuminate\Database\Eloquent\Model $tenant = null): string
+    {
+        $url = parent::getUrl($name, $parameters, $isAbsolute, $panel, $tenant);
+        return static::addPublicToUrl($url);
+    }
+
+    protected static function addPublicToUrl(string $url): string
+    {
+        if (str_contains($url, '/admin/') && !str_contains($url, '/admin/public/')) {
+            return str_replace('/admin/', '/admin/public/', $url);
+        }
+        return $url;
+    }
+
     public static function getPages(): array
     {
         return [
--- app/Filament/Resources/Recruitment/RecruitmentContractResource/Pages/ViewRecruitmentContract.php
+++ app/Filament/Resources/Recruitment/RecruitmentContractResource/Pages/ViewRecruitmentContract.php
@@ -10,6 +10,20 @@ class ViewRecruitmentContract extends ViewRecord
 {
     protected static string $resource = RecruitmentContractResource::class;
 
+    public static function getUrl(array $parameters = [], bool $isAbsolute = true, ?string $panel = null, ?\Illuminate\Database\Eloquent\Model $tenant = null): string
+    {
+        $url = parent::getUrl($parameters, $isAbsolute, $panel, $tenant);
+        return static::addPublicToUrl($url);
+    }
+
+    protected static function addPublicToUrl(string $url): string
+    {
+        if (str_contains($url, '/admin/') && !str_contains($url, '/admin/public/')) {
+            return str_replace('/admin/', '/admin/public/', $url);
+        }
+        return $url;
+    }
+
     protected function getHeaderActions(): array
     {
         return [
