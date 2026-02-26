a5e1f182b6450e8820650c210076c3e435113811
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-26 18:36:40 +0200

    qweqw

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -8,29 +8,6 @@ class Login extends BaseLogin
 {
     protected function getRedirectUrl(): string
     {
-        $url = \App\Filament\Pages\Dashboard::getUrl();
-        return static::addPublicToUrl($url);
-    }
-
-    protected static function addPublicToUrl(string $url): string
-    {
-        $parsed = parse_url($url);
-        $path = $parsed['path'] ?? '';
-        
-        if (str_contains($path, '/admin/') && !str_contains($path, '/public/admin/')) {
-            if (str_starts_with($path, '/public/')) {
-                $path = substr($path, 7);
-            }
-            $newPath = str_replace('/admin/', '/public/admin/', $path);
-            
-            $scheme = $parsed['scheme'] ?? 'https';
-            $host = $parsed['host'] ?? '';
-            $query = isset($parsed['query']) ? '?' . $parsed['query'] : '';
-            $fragment = isset($parsed['fragment']) ? '#' . $parsed['fragment'] : '';
-            
-            return $scheme . '://' . $host . $newPath . $query . $fragment;
-        }
-        
-        return $url;
+        return \App\Filament\Pages\Dashboard::getUrl();
     }
 }
