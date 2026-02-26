6a24079718c51dd7e52e9c4ccfaab083ec64ce65
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-26 18:10:58 +0200

    qweqw

---
+++ app/Filament/Actions/TableCreateAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Actions;
+
+use Filament\Tables\Actions\CreateAction as BaseCreateAction;
+
+class TableCreateAction extends BaseCreateAction
+{
+    protected function getUrl(): ?string
+    {
+        $url = parent::getUrl();
+        
+        if ($url === null) {
+            return null;
+        }
+        
+        return $this->addPublicToUrl($url);
+    }
+
+    protected function addPublicToUrl(string $url): string
+    {
+        $parsed = parse_url($url);
+        $path = $parsed['path'] ?? '';
+        
+        if (str_contains($path, '/admin/') && !str_contains($path, '/public/admin/')) {
+            if (str_starts_with($path, '/public/')) {
+                $path = substr($path, 7);
+            }
+            $newPath = str_replace('/admin/', '/public/admin/', $path);
+            
+            $scheme = $parsed['scheme'] ?? 'https';
+            $host = $parsed['host'] ?? '';
+            $query = isset($parsed['query']) ? '?' . $parsed['query'] : '';
+            $fragment = isset($parsed['fragment']) ? '#' . $parsed['fragment'] : '';
+            
+            return $scheme . '://' . $host . $newPath . $query . $fragment;
+        }
+        
+        return $url;
+    }
+}
