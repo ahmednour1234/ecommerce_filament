63140537881b6673a213ad62d1243ae2819f499d
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-03 20:52:17 +0200

    qweqw

--- app/Filament/Resources/Housing/Recruitment/RecruitmentHousingRequestResource.php
+++ app/Filament/Resources/Housing/Recruitment/RecruitmentHousingRequestResource.php
@@ -368,6 +368,6 @@ class RecruitmentHousingRequestResource extends Resource
 
     public static function shouldRegisterNavigation(): bool
     {
-        return static::canViewAny();
+        return false;
     }
 }
