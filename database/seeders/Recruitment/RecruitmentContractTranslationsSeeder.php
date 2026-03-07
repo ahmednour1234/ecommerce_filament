327356b9d59a743dfe0f80ad3336416d8f9ce4e6
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-07 22:21:39 +0200

    qweqw

--- database/seeders/Recruitment/RecruitmentContractTranslationsSeeder.php
+++ database/seeders/Recruitment/RecruitmentContractTranslationsSeeder.php
@@ -139,6 +139,7 @@ class RecruitmentContractTranslationsSeeder extends Seeder
             'recruitment_contract.status.contract_accepted_labor_ministry' => ['en' => 'Contract Accepted by Labor Ministry', 'ar' => 'قبول العقد من مكتب العمل الخارجي'],
             'recruitment_contract.status.sent_to_saudi_embassy' => ['en' => 'Sent to Saudi Embassy', 'ar' => 'إرسال التأشيرة إلى السفارة السعودية'],
             'recruitment_contract.status.visa_issued' => ['en' => 'Visa Issued', 'ar' => 'تم التفييز'],
+            'recruitment_contract.status.travel_permit_after_visa_issued' => ['en' => 'Travel Permit After Visa Issued', 'ar' => 'تصريح سفر بعد تم التفييز'],
             'recruitment_contract.status.waiting_flight_booking' => ['en' => 'Waiting Flight Booking', 'ar' => 'انتظار حجز تذكرة الطيران'],
             'recruitment_contract.status.arrival_scheduled' => ['en' => 'Arrival Scheduled', 'ar' => 'معاد الوصول'],
             'recruitment_contract.status.received' => ['en' => 'Received', 'ar' => 'تم الاستلام'],
