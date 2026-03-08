f9c4bc9f25a52482d132fc358b078dc2896c2f0e
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-08 06:25:34 +0200

    qweqw

--- app/Filament/Resources/ComplaintResource.php
+++ app/Filament/Resources/ComplaintResource.php
@@ -394,6 +394,34 @@ class ComplaintResource extends Resource
         ];
     }
 
+    public static function getUrl(string $name = 'index', array $parameters = [], bool $isAbsolute = true, ?string $panel = null, ?\Illuminate\Database\Eloquent\Model $tenant = null): string
+    {
+        $url = parent::getUrl($name, $parameters, $isAbsolute, $panel, $tenant);
+        return static::addPublicToUrl($url);
+    }
+
+    protected static function addPublicToUrl(string $url): string
+    {
+        $parsed = parse_url($url);
+        $path = $parsed['path'] ?? '';
+
+        if (str_contains($path, '/admin/') && !str_contains($path, '/public/admin/')) {
+            if (str_starts_with($path, '/public/')) {
+                $path = substr($path, 7);
+            }
+            $newPath = str_replace('/admin/', '/public/admin/', $path);
+
+            $scheme = $parsed['scheme'] ?? 'https';
+            $host = $parsed['host'] ?? '';
+            $query = isset($parsed['query']) ? '?' . $parsed['query'] : '';
+            $fragment = isset($parsed['fragment']) ? '#' . $parsed['fragment'] : '';
+
+            return $scheme . '://' . $host . $newPath . $query . $fragment;
+        }
+
+        return $url;
+    }
+
     public static function canViewAny(): bool
     {
         $user = auth()->user();
