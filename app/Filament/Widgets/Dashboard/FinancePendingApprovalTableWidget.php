5b432ed4eaffd1a3685c5da66c66cc4b129f663b
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-28 15:28:54 +0200

    first commit

--- app/Filament/Widgets/Dashboard/FinancePendingApprovalTableWidget.php
+++ app/Filament/Widgets/Dashboard/FinancePendingApprovalTableWidget.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 class FinancePendingApprovalTableWidget extends BaseWidget
 {
     protected static ?string $heading = 'آخر 5 معاملات تنتظر الموافقة';
+    protected static string $view = 'filament.widgets.finance-section-header-widget';
 
     protected int|string|array $columnSpan = 'full';
 
