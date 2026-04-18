e17f0cb6b1650c645dc1b402e2f658fe8259eee7
ahmednour1234 <ahmednour5999@gmail.com>
2026-04-18 20:07:05 +0200

    asdsda

--- resources/views/layouts/owner-dashboard.blade.php
+++ resources/views/layouts/owner-dashboard.blade.php
@@ -116,7 +116,7 @@
 
             <a href="{{ url('/admin/rental/rental-contracts') }}" class="nav-item">
                 <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/></svg>
-                <span>العقود</span>
+                <span>عقود الإيجار</span>
             </a>
 
             <div class="nav-divider"></div>
