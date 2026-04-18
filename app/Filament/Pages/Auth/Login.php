c17216c6552c939034ae16f39c87bd26a96c2478
ahmednour1234 <ahmednour5999@gmail.com>
2026-04-18 21:15:29 +0200

    asdsda

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -8,6 +8,12 @@ class Login extends BaseLogin
 {
     protected function getRedirectUrl(): string
     {
+        $user = \Illuminate\Support\Facades\Auth::user();
+
+        if ($user && $user->type === \App\Models\User::TYPE_COMPANY_OWNER) {
+            return route('owner.dashboard');
+        }
+
         return \App\Filament\Pages\Dashboard::getUrl();
     }
 }
